8246358121c11493b6c0c72bc2886a1e3054cfd2
Saurav Dan <sauravdan@yahoo.com>
2015-09-02 19:36:07 +0530

    Added custom markers
    Why:
    
    * Use Image as markers
    
    This change addresses the need by:
    
    * Using icon descriptor in marker parameter

---
+++ config.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $("#map").attr("src","https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=icon:http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=cafe%257C996600|color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=<API_KEY>");
+});
